e54fe87c3f13c288d45d30a29500a5a894d0a019
antonovmike <antonvma@outlook.com>
2022-10-01 13:34:10 +0500

    no file reading

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,19 @@
 extern crate ring;
 extern crate data_encoding;
 
-use yaml_rust::YamlLoader;
+// use yaml_rust::YamlLoader;
 
-use std::io::Read;
+// use std::io::Read;
 
 mod hashing;
 
 fn main() {
     hashing::hashing_pswd();
 
-    let mut file = std::fs::File::open("config.example.yaml").expect("Unable to open file");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .expect("Unable to read file");
-    let d = YamlLoader::load_from_str(&contents).unwrap();
-    println!("Read YAML string: {:?}", d);
+    // let mut file = std::fs::File::open("config.example.yaml").expect("Unable to open file");
+    // let mut contents = String::new();
+    // file.read_to_string(&mut contents)
+    //     .expect("Unable to read file");
+    // let d = YamlLoader::load_from_str(&contents).unwrap();
+    // println!("Read YAML string: {:?}", d);
 }
